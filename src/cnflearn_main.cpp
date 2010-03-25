267685d5669452d76f51ea95885322df6910eeeb
uchiumi <uchiumi@uchiumi-kei-nokonpyuta.local>
2010-03-25 23:51:09 +0900

    iteration = 50

--- src/cnflearn_main.cpp
+++ src/cnflearn_main.cpp
@@ -3,10 +3,10 @@
 
 int main(int argc, char **argv)
 {
-   Cnflearn cnf(*(argv+1), *(argv+2), 1000000);
+   Cnflearn cnf(*(argv+1), *(argv+2), 500000);
    cnf.setpenalty(0.0001,0.0001,0.0001);
    cnf.init();
-   cnf.learn(200,1);
+   cnf.learn(50,1);
    cnf.save(*(argv+3));
    return 0;
 }
